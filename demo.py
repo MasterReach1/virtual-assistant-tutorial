da491a8612508d0c3f75960d289aa717f3965805
Ethan Cox <77064058+MasterReach1@users.noreply.github.com>
2023-11-16 18:35:42 -0600

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+
+API_KEY = "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"
+
+client = OpenAI(
+    api_key=API_KEY
+)
+
+messages = []
+bot_role = input("What kind of virtual assistant would you like? >> ")
+messages.append({"role": "system", "content": bot_role})
+
+print("Virtual assistant successfully created!")
+
+while True:
+    gptMessage = input("Prompt your VA (or type QUIT) >> ")
+    messages.append({"role": "user", "content": gptMessage})
+
+    if gptMessage.lower != "quit":
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+
+        reply = completion.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+
+        print("\n" + reply + "\n")
+    else:
+        break
